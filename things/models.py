68c2ad10d2320d395cff85a2b25a8ceb250b9446
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-26 13:14:08 +0100

    Remove multiple validator= from quantity and replace with validate_quantity_range function.
    Additionally, complete migrations to database.

--- things/models.py
+++ things/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.exceptions import ValidationError
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.PositiveIntegerField(MinValueValidator(0), MaxValueValidator(100))
+    quantity = models.PositiveIntegerField(validator=[validate_quantity_range])
+
+    def validate_quantity_range(value):
+        if not (value <= 100 and value >= 0):
+            raise ValidationError()
 
 # Create your models here.
