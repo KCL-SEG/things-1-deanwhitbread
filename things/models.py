a46ffff8a912222101f04f94316131aaa10a3339
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-26 13:29:03 +0100

    Remove customer validator function and replace with predefined validators.

--- things/models.py
+++ things/models.py
@@ -1,13 +1,7 @@
 from django.db import models
-from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.PositiveIntegerField(validator=[validate_quantity_range])
-
-    def validate_quantity_range(value):
-        if not (value <= 100 and value >= 0):
-            raise ValidationError()
-
-# Create your models here.
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
