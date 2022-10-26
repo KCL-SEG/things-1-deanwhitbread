231177cb77bf882d2351ce3502b3610adbd68744
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-26 12:16:07 +0100

    Add model to Things app.
    Created a new Thing model to the Things app.
    Database migrations completed.

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Thing(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=80)
+    quantity = models.PositiveIntegerField()
+
 # Create your models here.
