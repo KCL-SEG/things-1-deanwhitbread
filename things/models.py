0b60daca78afe0c6cfb504b0fdb6773737189c23
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-26 13:03:59 +0100

    Add validators= to field options.

--- things/models.py
+++ things/models.py
@@ -4,6 +4,6 @@ from django.core.validators
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.PositiveIntegerField(MinValueValidator(0), MaxValueValidator(100))
+    quantity = models.PositiveIntegerField(validators=MinValueValidator(0), validators=MaxValueValidator(100))
 
 # Create your models here.
