b6c74c97eef5de7193923a97a57dc6b05dbb39f1
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-26 13:06:08 +0100

    Add specific imports from validators package.

--- things/models.py
+++ things/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
