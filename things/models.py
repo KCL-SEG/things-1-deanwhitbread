84e78b5d90b2371fef02aba0c86a435d8efeba81
Dean Whitbread <dean_whitbread@hotmail.com>
2022-10-26 13:07:17 +0100

    Remove validators= from field options.

--- things/models.py
+++ things/models.py
@@ -4,6 +4,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, blank=True)
-    quantity = models.PositiveIntegerField(validators=MinValueValidator(0), validators=MaxValueValidator(100))
+    quantity = models.PositiveIntegerField(MinValueValidator(0), MaxValueValidator(100))
 
 # Create your models here.
